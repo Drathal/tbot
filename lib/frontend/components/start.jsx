fcf59af12d5cc744cb7d6fd18ce5d14d7237893e
Markus Dethlefsen <privat@markus-dethlefsen.de>
2016-10-26 02:09:29 +0200

    refactor

--- lib/frontend/components/start.jsx
+++ lib/frontend/components/start.jsx
@@ -4,17 +4,20 @@ const {ipcRenderer} = require('electron')
 const component = ({ messages }) => (<div className={`main`}>
   <h1>tbot</h1>
   <button onClick={ () => ipcRenderer.send('start') } >{ messages.buttton_start }</button>
+  <button onClick={ () => ipcRenderer.send('stop') } >{ messages.buttton_stop }</button>
 </div>)
 
 component.propTypes = {
   messages: PropTypes.shape({
-    buttton_start: React.PropTypes.string
+    buttton_start: React.PropTypes.string,
+    buttton_stop: React.PropTypes.string
   }).isRequired
 }
 
 component.defaultProps = {
   messages: {
-    buttton_start: '#start#'
+    buttton_start: '#start#',
+    buttton_stop: '#stop#'
   }
 }
 
